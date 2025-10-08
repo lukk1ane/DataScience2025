85e8b4fa9ba383cc0e406af88f2c61309f548751
wi1dturtle <mixeildadianimziuri@gmail.com>
2025-10-08 14:03:59 +0400

    Delete Task10.py

--- Task10.py
+++
@@ -1,9 +0,0 @@
-def division(num1,num2):
-    try:
-        return num1/num2
-    except ZeroDivisionError:
-        print("Dvision by zero")
-
-print(division(1,0))
-print(division(2,1))
-print(division(0,1))
